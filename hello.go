aed11d9e0d14dd9d85dda2924950fbf932ac381f
muxuezi <muxuezi@gmail.com>
2015-04-05 15:35:48 +0800

    update

--- hello.go
+++ hello.go
@@ -2,8 +2,24 @@ package main
 
 import (
 	"fmt"
+	"string"
 )
 
+func nextInt(b []byte, i int) (int, int) {
+	for ; i < len(b) && !isDigit(b[i]); i++ {
+	}
+	x := 0
+	for ; i < len(b) && isDigit(b[i]); i++ {
+		x = x*10 + int(b[i]) - '0'
+	}
+	return x, i
+}
+
 func main() {
+	b := "0ll123"
+	for i := 0; i < len(b); {
+		x, i = nextInt(b, i)
+		fmt.Println(x)
+	}
 	fmt.Println("hello, world.\n")
 }
---
+++ hellos.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	printf("1,2,3");
+	return 0;
+}
